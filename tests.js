15163bb21fc7f5203c5a0b8d0fe62d7fad51e1da
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-03-08 19:32:31 +0700

    First commit.

--- tests.js
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-import {name as appName} from "./quitr.js";
-
-describe("quitr", () => {
-  it("should export its name", () => {
-    assert.strictEqual(appName, "quitr");
-  });
-});
